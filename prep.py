43b9617411541d7405ac755de7dce391a089c440
J. Brandon Walker <88942620+walkerjb02@users.noreply.github.com>
2022-08-21 21:19:36 -0400

    Update prep.py

--- prep.py
+++ prep.py
@@ -1,33 +1,36 @@
+from getloc import *
 from utils import *
-from get_loc import *
-import numpy as np
 
 class Prep(Root):
     def get_bounds(self):
-        bounds = get_loc().bounds(utils().get_section(48, 0, self.height, self.pixels, iterindex=-1),dimension=1)
-        revisedbounds = get_loc().stitch_bounds(bounds)
-        finalizedbounds = get_loc().assemble_bounds(bounds=revisedbounds)
-        utils().to_json("Len", "Bounds", finalizedbounds)
+        bounds = GetLoc().bounds(Utils().get_section(48, 48, 0, self.height, self.pixels, iterindex=-1), dimension=1)
+        revisedbounds = GetLoc().stitch_bounds(bounds)
+        finalizedbounds = GetLoc().assemble_bounds(bounds=revisedbounds)
+        Utils().to_json("Len", "Bounds", finalizedbounds)
         return finalizedbounds
 
-    def get_words(self, bounds):
-        get_loc().condense()
+    def get_words(self, bounds, transformation, widthstart, widthstop, dimension):
+        # Condense section into array thats stored into Storage
+        sections = GetLoc().condense(transformation, widthstart, widthstop)
         idx = 0
+        wordloc = []
         while idx < len(bounds):
-            with open(f'Storage{idx}.json', 'r') as file:
-                section = json.loads(file.read())["Sections"]
-                words = get_loc().bounds(section,dimension=2)
-                words = get_loc().clear_noise(words)
-                print(words)
+            # Feeding condensed section stored in Storages into GetLoc
+            words = GetLoc().bounds(sections[idx], dimension=dimension)
+            words = GetLoc().clear_noise(words)
+            wordloc.append(words)
+            Utils().to_json(idx, "Words", words)
+            idx += 1
+        return wordloc
 
-                utils().to_json(idx, "Sections", words)
-                idx += 1
+    def get_letters(self, bounds):
+        letters = self.get_words(bounds, 0, 0, self.width, 3)
+        letters = GetLoc().cut_long(letters)
+        return letters
 
-    def run(self):
-        utils().clear_json(40)
+    def prep_main(self):
+        Utils().clear_json(0)
         bounds = self.get_bounds()
-        words = self.get_words(bounds)
-        print(words)
-        idx = 0
-        with open(f'Storage{idx}.json','r+') as file:
-            wordbounds = json.loads(file.read())["Bounds"]
+        words = self.get_letters(bounds)
+        finalized = Utils().get_for_network(words)
+        return finalized
